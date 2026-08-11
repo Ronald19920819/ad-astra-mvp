4faf8a7f660e8c888745dde8f5b852506602ef7f
Ronald Petersen <ronaldpetersen25@gmail.com>
2026-08-11 10:09:48 +0200

    Enable learner live classroom diagnostics

--- components/subjects/SubjectLiveClassroomPage.tsx
+++ components/subjects/SubjectLiveClassroomPage.tsx
@@ -110,6 +110,8 @@ export async function SubjectLiveClassroomPage({
             <LiveClassroomPlayer
               subjectColour={subject.colourTheme.primary}
               subjectSoftBackground={subject.colourTheme.softBackground}
+              requireExplicitAudioJoin
+              showLearnerSupportInfo
             />
           </section>
 
